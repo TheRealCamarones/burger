1519f6a1143c4a96dafb2a90c8449b0123b80017
Camarones <cameron_walther@yahoo.com>
2019-05-13 16:47:34 -0400

    finished orm and burger model

--- config/orm.js
+++ config/orm.js
@@ -1,16 +1,79 @@
 var connection = require("./connection");
 
-var orm = {
-    selectAll: function(table) {
-      var queryString = `SELECT * FROM ??`;
-      connection.query(queryString, [table])
-    },
-    insertOne: function() {
+// fun helper function from in class activity to avoid sql injection
+function printQuestionMarks(num) {
+    var arr = [];
 
-    },
-    updateOne: function() {
-
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
     }
+
+    return arr.toString();
+}
+
+// and another helper function to convert object key value pairs to SQL syntax
+function objtoSql(ob) {
+    var arr = [];
+
+    for (var key in ob) {
+        var value = ob[key];
+
+        if (Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === 'string' && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            arr.push(key + "=" + value);
+        }
+    }
+
+    return arr.toString();
+}
+
+var orm = {
+    selectAll: function(table, cb) {
+      var queryString = `SELECT * FROM ` + table + ";";
+      connection.query(queryString, function(err, result) {
+          if (err) throw err;
+          cb(result);
+      })
+    },
+    insertOne: function(table, cols, vals, cb) {
+      var queryString = `INSERT INTO ` + table;
+
+      queryString += " (";
+      queryString += cols.toString();
+      queryString += ") ";
+      queryString += "VALUES (";
+      queryString += printQuestionMarks(vals.length);
+      queryString += ") ";
+
+      console.log(queryString);
+
+      connection.query(queryString, vals, function(err, result) {
+          if (err) {
+              throw err;
+          }
+
+          cb(result);
+      })
+    },
+    updateOne: function(table, objColVals, condition, cb) {
+      var queryString = `UPDATE ` + table;
+
+      queryString += ` SET `;
+      queryString += objtoSql(objColVals);
+      queryString += " WHERE ";
+      queryString += condition;
+
+      console.log(queryString);
+      connection.query(queryString), function(err, result) {
+          if (err) {
+              throw err;
+          }
+
+          cb(result);
+      }
+        }
 };
 
 module.exports = orm;
--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,21 @@
 var orm = require("../config/orm");
 
 var burger = {
-    
+    selectAll: function(cb) {
+      orm.selectAll("burgers", function(res) {
+        cb(res);
+      });
+    },
+    insertOne: function(cb) {
+      orm.insertOne("burgers", cols, vals, function(res) {
+          cb(res);
+      });
+    },
+    updateOne: function(cb) {
+        orm.updateOne("burgers", objColVals, condition, function(res) {
+            cb(res);
+        })
+    }
 };
 
 module.exports = burger;
