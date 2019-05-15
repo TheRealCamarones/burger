b2ab4b5707b09be92d4403096b2e0b648e9fc894
Camarones <cameron_walther@yahoo.com>
2019-05-14 23:48:47 -0400

    not displaying the html the way I want but everything else is done

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,8 +1,48 @@
 var express = require("express");
 var router = express.Router();
 
+// import the burger model to use the database functions
+var burger = require("../models/burger.js");
+
+// create the routes and add logic to execute
 router.get('/', function(req, res) {
-    res.render('index');
+  burger.selectAll(function(data) {
+      var burgerObject = {
+          burgers: data
+      };
+      console.log(burgerObject);
+      res.render('index');
+  });
+});
+
+router.post("/api/burgers", function(req, res) {
+    burger.insertOne([
+        "burger_name", "devoured"
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function(result) {
+        // return id of new burger
+        res.json({
+            id: result.insertId
+        });
+    });
+});
+
+router.put("/api/cats/:id", function(req, res) {
+    var status = "id = " + req.params.id;
+
+    console.log("status", status);
+
+    burger.update({
+        devoured: req.body.devoured
+    }, status, function(result) {
+        // if nothing was changed this will return an error
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
 });
 
 module.exports = router;
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,41 @@
+$(function() {
+    $(".change-eaten").on("click", function(event) {
+        var id = $(this).data("id");
+        var newEaten = $(this).data("newEaten");
+
+        var newEatenState = {
+            devoured: newEaten
+        };
+
+        // then use the API to send the updated devoured state
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newEatenState
+        }).then(
+            function() {
+                console.log("changed eaten to", newEaten);
+                // reload the page to update the list
+                location.reload();
+            }
+        );
+    });
+
+    $(".create-form").on("submit", function(event) {
+        event.preventDefault();
+
+        var newBurger = {
+            burger_name: $("#burg").val().trim(),
+            devoured: $("[name=devoured]:checked").val().trim()
+        };
+
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function() {
+                console.log("created new burger");
+                location.reload();
+            }
+        );
+    });
+});
