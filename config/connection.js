abeae2fafaea4326cad106a5e09c58ecd4f0c3a9
Camarones <cameron_walther@yahoo.com>
2019-05-30 09:29:55 -0400

    Patrick helped me set up my heroku deployment with Jaws DB, should fix deployment problems, tested and works locally

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,20 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "potpourrigizmo",
-    database: "burgers_db"
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+    // Database is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // Database is local
+    connection = mysql.createConnection({
+        host: "thh2lzgakldp794r.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        port: 3306,
+        user: "fq059hylvcnaxb1i",
+        password: "h1kh6n40mn36w6h2",
+        database: "ooc4uzbbgyvdx0zi"
+    })
+};
+
 
 connection.connect(function(err) {
     if (err) {
